77e85b0003fe468e1dfa7028e4ce6bb464c69496
Geek <qiu0089@foxmail.com>
2024-08-23 10:47:20 +0800

    🔧 other(res): 添加 GeoTIFF 数据源文件(Git LFS)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from tasks.workspace import create_workspace
 from tasks.coveragestore import create_coverage_store
 from tasks.layer import create_layer
@@ -8,3 +10,6 @@ if __name__ == '__main__':
         if create_coverage_store() == 201:  # CoverageStore 创建成功
             create_layer()  # 尝试创建 layer
 
+    # 定义GeoTIFF文件路径
+    tiff_file_path = os.path.join(os.path.dirname(__file__), 'data', 'shanghai.tiff')
+    print(tiff_file_path)
